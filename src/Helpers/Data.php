d06db05f2fd12968c6bc78f38283585b9b321605
Ben <ben@alt-design.net>
2023-10-30 20:17:44 +0000

    Tidying, add field descriptions and length counts

--- src/Helpers/Data.php
+++ src/Helpers/Data.php
@@ -3,18 +3,42 @@
 use Statamic\Facades\YAML;
 use Statamic\Filesystem\Manager;
 
+/**
+ * Class Data
+ *
+ * @package  AltDesign\AltSeo
+ * @author   Ben Harvey <ben@alt-design.net>, Natalie Higgins <natalie@alt-design.net>
+ * @license  Copyright (C) Alt Design Limited - All Rights Reserved - licensed under the MIT license
+ * @link     https://alt-design.net
+ */
 class Data
 {
+    /**
+     * @var - The manager class for Statamic
+     */
     public $type;
+
+    /**
+     * @var Manager
+     */
     public $manager;
+    /**
+     * @var mixed|null
+     */
     public $currentFile;
+    /**
+     * @var array
+     */
     public $data;
 
+    /**
+     * @param $type
+     */
     public function __construct($type)
     {
         $this->type = $type;
 
-        // New up Stat File Manager
+        // New up Statamic File Manager
         $this->manager = new Manager();
 
         // Grab the current file we're working with
@@ -23,14 +47,22 @@ class Data
         $this->data = Yaml::parse($this->currentFile);
     }
 
+    /**
+     * Gets data from the Yaml file by key.
+     *
+     * @param $key
+     * @return mixed|null
+     */
     public function get($key)
     {
-        if (!isset($this->data[$key])) {
-            return null;
-        }
-        return $this->data[$key];
+        return $this->data[$key] ?? null;
     }
 
+    /**
+     * @param $key
+     * @param $value
+     * @return void
+     */
     public function set($key, $value)
     {
         $this->data[$key] = $value;
@@ -38,11 +70,18 @@ class Data
         Yaml::dump($this->data, $this->currentFile);
     }
 
+    /**
+     * @return array
+     */
     public function all()
     {
         return $this->data;
     }
 
+    /**
+     * @param $data
+     * @return void
+     */
     public function setAll($data)
     {
         $this->data = $data;
