0fc41a229d40560fc0af1cc5f13d752cce46136d
benammi <benammi@alt-design.net>
2023-11-17 12:10:44 +0000

    Per collection asset container handles

--- src/Events/Seo.php
+++ src/Events/Seo.php
@@ -2,6 +2,8 @@
 
 use Statamic\Events;
 use Statamic\Facades\Blueprint;
+use Statamic\Facades\Entry;
+use AltDesign\AltSeo\Helpers\Data;
 
 /**
  * Class Seo
@@ -48,6 +50,30 @@ class Seo
         // Grab the tabs - there may be a better way of doing this?
         $blueprint = Blueprint::setDirectory(__DIR__ . '/../../resources/blueprints')->find('seo');
         $blueprintReady = $event->blueprint->contents();
+        $data = new Data('settings');
+
+        //Global override
+        if ($data->get('alt_seo_asset_container') !== null)
+        {
+            $contents = $blueprint->contents();
+            $contents['tabs']['alt_seo']['sections'][0]['fields'][6]['field']['container'] = $data->get('alt_seo_asset_container');
+            $blueprint->setContents($contents);
+        }
+        
+        //Pre-collection overridex
+        if ($data->get('alt_seo_collection_asset_containers'))
+        {
+            $containerSettings = $data->get('alt_seo_collection_asset_containers');
+            $thisEntryHandle = $event->entry->collection->handle;
+            $contents = $blueprint->contents();
+            foreach($containerSettings as $setting) {
+                if ($setting['collection'] === $thisEntryHandle)
+                {
+                    $contents['tabs']['alt_seo']['sections'][0]['fields'][6]['field']['container'] = $setting['asset_handle'] ?? 'assets';
+                }
+            }
+            $blueprint->setContents($contents);
+        }
         $blueprintReady['tabs'] = array_merge($blueprintReady['tabs'], $blueprint->contents()['tabs']);
 
         // Set the contents
